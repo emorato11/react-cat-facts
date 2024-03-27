b1a3a2a1d069c164c25ffd3ebad7c53bfc20c762
Enrique Morato <quiquemorato@gmail.com>
2024-03-27 11:05:29 +0100

    Add config for relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(path.join(__dirname, "/src/")),
+      },
+    ],
+  },
 });
